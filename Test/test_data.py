25fb1215a0f514d7aaeff3f3ef6d4966f49c8038
Mathias Depussay <mathias.depussay@etu.univ-tours.fr>
2025-04-25 16:49:45 +0200

    new file:   Test/__pycache__/test_data.cpython-313-pytest-8.3.5.pyc 	modified:   Test/test_data.py 	modified:   pyproject.toml 	new file:   src/__pycache__/__init__.cpython-313.pyc 	new file:   src/__pycache__/app.cpython-313.pyc 	modified:   src/__pycache__/data.cpython-313.pyc 	modified:   uv.lock

---
+++ Test/test_data.py
@@ -0,0 +1,23 @@
+import pytest 
+from src.data import *
+from src.affichage import afficherCarte
+from src.app import menu_terminal
+
+def test_modification_liaison_ameliore_flot():
+    original = ListeLiaison[:]
+    modifiee = [l if l.depart != "A" or l.arrivee != "E" else liaison("A", "E", 15) for l in ListeLiaison]
+    
+    flot_avant, _ = ReseauHydraulique(ListeNoeuds, original).calculerFlotMaximal()
+    flot_apres, _ = ReseauHydraulique(ListeNoeuds, modifiee).calculerFlotMaximal()
+    
+    assert flot_apres.flow_value > flot_avant.flow_value
+
+
+def test_liaison_inexistante():
+    assert not liaison_existe("Z", "Q", ListeLiaison)
+
+def test_creation_noeud():
+    n = noeud("A", "source", 10)
+    assert n.nom == "A"
+    assert n.type == "source"
+    assert n.capaciteMax == 10
