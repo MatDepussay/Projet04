79abc71fe21de80ebb49d1b4c911110a694e63ac
Mathias Depussay <mathias.depussay@etu.univ-tours.fr>
2025-04-25 17:26:47 +0200

    modified:   .coverage 	modified:   Test/__pycache__/test_data.cpython-313-pytest-8.3.5.pyc 	modified:   Test/test_data.py 	modified:   src/__pycache__/affichage.cpython-313.pyc

--- Test/test_data.py
+++ Test/test_data.py
@@ -4,6 +4,10 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 
 import pytest 
 from src.data import *
+import builtins
+from io import StringIO
+from src.app import menu_terminal
+
 
 def test_modification_liaison_ameliore_flot():
     original = ListeLiaison[:]
@@ -54,3 +58,24 @@ def test_optimiser_liaisons_priorise_meilleure_liaison():
 
     for _, cap, _ in travaux:
         assert 1 <= cap <= 20
+
+
+def test_menu_travaux(monkeypatch, capsys):
+    inputs = iter([
+        "2",    
+        "A", "E",
+        "I", "L",
+        "n",    
+        "3"     
+    ])
+    
+    monkeypatch.setattr("builtins.input", lambda _: next(inputs)) #monkeypatch input pour simuler les entrées utilisateur
+    
+    
+    menu_terminal()
+    
+    out, _ = capsys.readouterr()
+ 
+    assert "Optimisation de l’ordre des travaux" in out
+    assert "Travaux #1" in out
+    assert "Au revoir" in out
